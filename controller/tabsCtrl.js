b27fdad56231a28778bd5d6d60472bb623205c02
Viacheslav <trussss@yandex.ru>
2016-08-20 02:24:21 +0300

    some fixes

---
+++ controller/tabsCtrl.js
@@ -0,0 +1,5 @@
+xFapp.controller("tabsCtrl", function($scope)  {
+	$scope.tabsArr = [{title: 'Contributors table', url: '/contributors'}, {title: 'GitHub forks table', url: '/xFormation'}];
+});
+
+
